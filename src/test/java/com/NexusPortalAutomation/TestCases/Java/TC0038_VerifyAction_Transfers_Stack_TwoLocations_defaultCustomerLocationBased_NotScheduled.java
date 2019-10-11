f1d4032b0d3cf2978825133f996f939ee611d68e
Talha <talharahim@gmail.com>
2019-10-11 07:51:24 -0500

    updates

--- src/test/java/com/NexusPortalAutomation/TestCases/Java/TC0038_VerifyAction_Transfers_Stack_TwoLocations_defaultCustomerLocationBased_NotScheduled.java
+++ src/test/java/com/NexusPortalAutomation/TestCases/Java/TC0038_VerifyAction_Transfers_Stack_TwoLocations_defaultCustomerLocationBased_NotScheduled.java
@@ -37,9 +37,11 @@ public class TC0038_VerifyAction_Transfers_Stack_TwoLocations_defaultCustomerLoc
 	public String[] task = {"Meter Reading-electric", "Charge New Customer","Property Transfer", "Deposit Request-new Customer", "Deposit Payment-new Customer E","Prepayment Required-new Custom"};
 	
 	public String serverUrl = getDrillbackServerUrl();
-
+/*
+ * There is an issue with this test and Removed from the Test suit 
+ */
 //This Test will test the search by Customer ID
-	@Test(priority = 1)
+	//@Test(priority = 1)
 	public void VerifyAction_Transfers_Stack_TwoLocations_defaultCustomerLocationBased_NotScheduled() throws IOException, InterruptedException, ClassNotFoundException,
 			SQLServerException, SQLException, ParseException {
 		DashBoardSearch dbSrch = new DashBoardSearch(driver);
@@ -49,105 +51,105 @@ public class TC0038_VerifyAction_Transfers_Stack_TwoLocations_defaultCustomerLoc
 		sql.deleteServiceorderHistory(locationID);
 		sql.deleteServiceorder(locationID2);
 		sql.deleteServiceorderHistory(locationID2);
-//		login();
-//		dbSrch.enterSearchText(locationID);
-//		dbSrch.clickCustomerName();
-//		// Verify Customer Location Id Updated for Test
-//		CommonMethods.VerifyString(locationID, dashBoard.GetLoggedCustomerlocationID());
-//		dashBoard.clickActionDropDown();
-//		dashBoard.clickActionDropDown_TransferService();
-//		dashBoard.SelectTransferType_Transfer_Stop();
-//		String moveOutrequestedDate = dashBoard.startService_getRequestedDate();
-//		// Entering data for Move Out
-//		// Scroll down
-//		dashBoard.submitStartStopServiceTransferOrder(moveOutrequestedDate, "", "TRANSFER", defaultCustomer1, "");
-//		// Verify Updated details IN SERVICE TAB order number from database
-//		dashBoard.ClickServiceOrderLink();
-//		dashBoard.ClickServOrder1();
-//		String ServiceOrder = dashBoard.getserviceOrderNum();
-//		String[] arrOfStr = moveOutrequestedDate.split(" ", 2);
-//		String moveOutstart_dt = arrOfStr[0];
-//		DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-//		Date date = (Date) formatter.parse(moveOutstart_dt);
-//		SimpleDateFormat newFormat = new SimpleDateFormat("MMM d, yyyy");
-//		String moveOutstart_dtfinalString = newFormat.format(date);
-//		dashBoard.clickrefreshPage();
-//
-////		/*
-////		// Adding Second Service with two dates ahead of current date
-////		*/
-////		dashBoard.clickActionDropDown();
-////		dashBoard.clickActionDropDown_TransferService();
-////		dashBoard.SelectTransferType_Transfer_Stop();
-////		dashBoard.verifyServiceWarningSingleSO(ServiceOrder);
-////		Calendar c = Calendar.getInstance();
-////		c.setTime(newFormat.parse(moveOutstart_dtfinalString));
-////		c.add(Calendar.DAY_OF_MONTH, 2);
-////		// Date after adding the days to the given date
-////		String newDate = newFormat.format(c.getTime());
-////		// Displaying the new Date after addition of Days
-////		System.out.println("Date after Addition of two days " + newDate);
-////		// Entering data for Move Out
-////		// Scroll down
-////   		//String moveInCustomer = "Mr. VACANT VACANT";
-////   		dashBoard.submitStartStopServiceTransferOrder(moveOutrequestedDate, "", "TRANSFER", defaultCustomer1, "");
-////		dashBoard.clickrefreshPage();
-////		dashBoard.ClickServiceOrderLink();
-////		dashBoard.ClickServOrder1();
-//		
-//		//NAVIGATE TO SECOND LOCATION
-//		//String ServiceOrder2 = dashBoard.getserviceOrderNum();
-//		dashBoard.clickrefreshPage();
-//		
-//		dashBoard.enterDashBoardSearch(locationID2);
-//		dashBoard.clickDashBoardSearchResult1();
-//		
-//		dashBoard.clickActionDropDown();
-//		dashBoard.clickActionDropDown_TransferService();
-//		dashBoard.SelectTransferType_Transfer();
-//		dashBoard.enterRequest("Transfer");
-//		// Move Out
-//		moveOutrequestedDate = dashBoard.Movin_getMoveOutRequestedDate();
-//		dashBoard.verifydefaultCustomer(defaultCustomer2);
-//		dashBoard.enterdefaultCustomer(defaultCustomer2);
-//		dashBoard.enterDescription("AUTOMATION TEST");
-//		// Move In
-//		dashBoard.ClickMoveIn();
-//		Calendar c1 = Calendar.getInstance();
-//		c1.add(Calendar.DAY_OF_MONTH, 1);
-//		// Date after adding the days to the given date
-//		String newDate2 = newFormat.format(c1.getTime());
-//		//dashBoard.Movin_EnterMoveToScheduleDate(newDate2);
-//		Thread.sleep(1000);
-//		dashBoard.Movin_EnterRequest("TRANSFER");
-//		dashBoard.Movin_EnterLocation(locationID);
-//		dashBoard.Movin_EnterDescription("Move in from location "+locationID);
-//		dashBoard.Click_MoveInSubmit();
-//		Thread.sleep(1000);
-//		dashBoard.verifySubmitMessage(Message);
-//		dashBoard.ClickDone();
-//		
-//		dashBoard.enterDashBoardSearch(locationID);
-//		dashBoard.clickDashBoardSearchResult1();
-//		//Navigate to Service Order
-//		dashBoard.clickrefreshPage();
-//		dashBoard.ClickServiceOrderLink();
-//		dashBoard.ClickServOrder1();
-//		String moveOutCustomer = "Mr. Test Mid Customer Suf";
-//		String moveInCustomer = "Mr. Automation Mate (0000011111)";
-//		dashBoard.verifyServiceOrderdetails(moveOutCustomer,moveOutCustomer,moveInCustomer,"Not Scheduled",moveOutstart_dtfinalString,Task,locationID);
-//		
-//		//Verifying Service Order 2
-//		dashBoard.ClickServOrder2();
-//		moveOutCustomer = "Mr. Talha Rahim";
-//		moveInCustomer = "Mr. Talha Rahim (03332301204)";
-//		dashBoard.verifyServiceOrderdetails(moveOutCustomer,moveOutCustomer,moveInCustomer,"Not Scheduled",moveOutstart_dtfinalString,Task,locationID);
-//		// Verifying Third Transfer order in the stack
-//// 		dashBoard.ClickServOrder3();
-//// 	   	moveOutCustomer = "Mr. Automation Mate";
-//// 		moveInCustomer = "Mr. Talha Rahim (03332301204)";
-////		dashBoard.verifyServiceOrderdetails(moveOutCustomer,moveOutCustomer,moveInCustomer,"Not Scheduled",moveOutstart_dtfinalString,Task,locationID);
-//		dashBoard.logout();
+		login();
+		dbSrch.enterSearchText(locationID);
+		dbSrch.clickCustomerName();
+		// Verify Customer Location Id Updated for Test
+		CommonMethods.verifyString(locationID, dashBoard.getLoggedCustomerLocationId());
+		dashBoard.clickActionDropDown();
+		dashBoard.clickActionDropDown_TransferService();
+		dashBoard.selectTransferStop();
+		String moveOutrequestedDate = dashBoard.startService_getRequestedDate();
+		// Entering data for Move Out
+		// Scroll down
+		dashBoard.submitStartStopServiceTransferOrder(moveOutrequestedDate, "TRANSFER", defaultCustomer2, defaultCustomer1,"");
+		// Verify Updated details IN SERVICE TAB order number from database
+		dashBoard.clickServiceorderLink();
+		dashBoard.clickServiceorder1();
+		String ServiceOrder = dashBoard.getserviceOrderNum();
+		String[] arrOfStr = moveOutrequestedDate.split(" ", 2);
+		String moveOutstart_dt = arrOfStr[0];
+		DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		Date date = (Date) formatter.parse(moveOutstart_dt);
+		SimpleDateFormat newFormat = new SimpleDateFormat("MMM d, yyyy");
+		String moveOutstart_dtfinalString = newFormat.format(date);
+		dashBoard.clickrefreshPage();
+
+		/*
+		 Adding Second Service with two dates ahead of current date
+		*/
+		dashBoard.clickActionDropDown();
+		dashBoard.clickActionDropDown_TransferService();
+		dashBoard.selectTransferStop();
+		dashBoard.verifyServiceWarningSingleSO(ServiceOrder);
+		Calendar c = Calendar.getInstance();
+		c.setTime(newFormat.parse(moveOutstart_dtfinalString));
+		c.add(Calendar.DAY_OF_MONTH, 2);
+		// Date after adding the days to the given date
+		String newDate = newFormat.format(c.getTime());
+		// Displaying the new Date after addition of Days
+		System.out.println("Date after Addition of two days " + newDate);
+		// Entering data for Move Out
+		// Scroll down
+   		String moveInCustomer = "Mr. VACANT VACANT";
+   		dashBoard.submitStartStopServiceTransferOrder(moveOutrequestedDate, "TRANSFER", defaultCustomer2, defaultCustomer1,"");
+		dashBoard.clickrefreshPage();
+		dashBoard.clickServiceorderLink();
+		dashBoard.clickServiceorder1();
+		
+		//NAVIGATE TO SECOND LOCATION
+		String ServiceOrder2 = dashBoard.getserviceOrderNum();
+		dashBoard.clickrefreshPage();
+		
+		dashBoard.enterDashBoardSearch(locationID2);
+		dashBoard.clickDashBoardSearchResult1();
+		
+		dashBoard.clickActionDropDown();
+		dashBoard.clickActionDropDown_TransferService();
+		dashBoard.selectTransferTypeTransfer();
+		dashBoard.enterRequest("Transfer");
+		// Move Out
+		moveOutrequestedDate = dashBoard.Movin_getMoveOutRequestedDate();
+		dashBoard.verifydefaultCustomer(defaultCustomer2);
+		dashBoard.enterdefaultCustomer(defaultCustomer2);
+		dashBoard.enterDescription("AUTOMATION TEST");
+		// Move In
+		dashBoard.ClickMoveTo();
+		Calendar c1 = Calendar.getInstance();
+		c1.add(Calendar.DAY_OF_MONTH, 1);
+		 //Date after adding the days to the given date
+		String newDate2 = newFormat.format(c1.getTime());
+		dashBoard.movinEnterMoveToScheduleDate(newDate2);
+		Thread.sleep(1000);
+		dashBoard.movinEnterRequest("TRANSFER");
+		dashBoard.movinEnterLocation(locationID);
+		dashBoard.movinEnterDescription("Move in from location "+locationID);
+		dashBoard.clickMoveInSubmit();
+		Thread.sleep(1000);
+		dashBoard.verifySubmitMessage(message);
+		dashBoard.clickDone();
+		
+		dashBoard.enterDashBoardSearch(locationID);
+		dashBoard.clickDashBoardSearchResult1();
+		//Navigate to Service Order
+		dashBoard.clickrefreshPage();
+		dashBoard.clickServiceorderLink();
+		dashBoard.clickServiceorder1();
+		String moveOutCustomer = "Mr. Test Mid Customer Suf";
+		moveInCustomer = "Mr. Automation Mate (0000011111)";
+		dashBoard.verifyServiceOrderdetails(moveOutCustomer,moveOutCustomer,moveInCustomer,"Not Scheduled",moveOutstart_dtfinalString,task,locationID);
+		
+		//Verifying Service Order 2
+		dashBoard.clickServiceorder2();
+		moveOutCustomer = "Mr. Talha Rahim";
+		moveInCustomer = "Mr. Talha Rahim (03332301204)";
+		dashBoard.verifyServiceOrderdetails(moveOutCustomer,moveOutCustomer,moveInCustomer,"Not Scheduled",moveOutstart_dtfinalString,task,locationID);
+		// Verifying Third Transfer order in the stack
+ 		dashBoard.clickServiceorder3();
+ 	   	moveOutCustomer = "Mr. Automation Mate";
+ 		moveInCustomer = "Mr. Talha Rahim (03332301204)";
+		dashBoard.verifyServiceOrderdetails(moveOutCustomer,moveOutCustomer,moveInCustomer,"Not Scheduled",moveOutstart_dtfinalString,task,locationID);
+		dashBoard.logout();
 	}
 
 }
